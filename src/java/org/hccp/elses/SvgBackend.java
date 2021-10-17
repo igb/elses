e2c3420fc1fed90a91b6705e100f8d31f8bb844b
Ian Brown <igb@hccp.org>
2021-10-17 07:29:21 -0700

    adding samples and references

--- src/java/org/hccp/elses/SvgBackend.java
+++ src/java/org/hccp/elses/SvgBackend.java
@@ -1,17 +1,25 @@
 package org.hccp.elses;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static org.hccp.elses.TokenType.*;
 
 public class SvgBackend extends Backend {
 
+    HashSet<String> record = new HashSet<>();
+
     StringBuffer outputBuffer = new StringBuffer();
 
 
     @Override
     public void intepret(List<Expr.Literal> ilr, Context context) {
-        outputBuffer.append("<svg xmlns=\"http://www.w3.org/2000/svg\">\n");
+        outputBuffer.append("<svg xmlns=\"http://www.w3.org/2000/svg\"" +
+                "   height=\"297.0mm\"\n" +
+                "   width=\"420.0mm\"\n" +
+                "   viewBox=\"0 0 420.0 297.0 \"\n" +
+                "   version=\"1.1\"" +
+                ">\n");
         for (int i = 0; i < ilr.size(); i++) {
             Expr.Literal instruction = ilr.get(i);
             if (instruction.value instanceof TokenType) {
@@ -19,7 +27,13 @@ public class SvgBackend extends Backend {
                 switch (value) {
                     case FORWARD:
                         System.out.println("draw line from " + context.getCurrentX() + ", " + context.getCurrentX() + " to " + calculateNewX(context) + ", " + calculateNewY(context));
-                        outputBuffer.append(String.format("<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"black\" />\n", context.getCurrentX(), context.getCurrentY(), calculateNewX(context), calculateNewY(context)));
+                        String line = String.format("<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" stroke=\"black\" />\n", context.getCurrentX(), context.getCurrentY(), calculateNewX(context), calculateNewY(context));
+
+                        //optimize output by eliminating duplicate paths
+                        if (!record.contains(line)) {
+                            outputBuffer.append(line);
+                            record.add(line);
+                        }
                         context.set(Context.CURRENT_X_POS, calculateNewX(context));
                         context.set(Context.CURRENT_Y_POS, calculateNewY(context));
                         break;
@@ -30,7 +44,13 @@ public class SvgBackend extends Backend {
                         break;
                     case DOT:
                         System.out.println("draw a dot at " + context.getCurrentX() + ", " + context.getCurrentX());
-                        outputBuffer.append(String.format("<circle cx=\"%f\" cy=\"%f\" r=\"%f\"/>\n", context.getCurrentX(), context.getCurrentY(), context.getDotRadius()));
+                        String dot = String.format("<circle cx=\"%f\" cy=\"%f\" r=\"%f\"/>\n", context.getCurrentX(), context.getCurrentY(), context.getDotRadius());
+
+                        //optimize output by eliminating duplicate paths
+                        if (!record.contains(dot)) {
+                           outputBuffer.append(dot);
+                           record.add(dot);
+                        }
                         context.set(Context.CURRENT_X_POS, calculateNewX(context));
                         context.set(Context.CURRENT_Y_POS, calculateNewY(context));
                         break;
